9dda38ec7c887a7a725cc6e70dab9301373e4dea
Eliard85 <79087364+Eliard85@users.noreply.github.com>
2025-07-20 14:29:12 +0300

    Update cpustart.sh

--- cpustart.sh
+++ cpustart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 t=$(( $(nproc) / 3 ))
 for ((i = 1; i <= t; i++)); do
-    screen -S cpu$((i)) -dm bash -c '/cpu.sh'
+    screen -S cpu$((i)) -dm bash -c '/root/jar/cpu.sh'
     sleep 5
 done
