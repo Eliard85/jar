b567c7645e9c4c8d2117a00c2c58e66907b9c2ff
Eliard85 <79087364+Eliard85@users.noreply.github.com>
2025-07-22 00:06:03 +0300

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WALLET="4D9j91yffNr9uMvucKckgknSnXD8f13a45"
 while [ 1 ]; do
-    java -jar -Xmx8G /root/jar/cpu.jar -u $WALLET -h 146.103.50.122 -p x -t 3 -P 5001
+    java -jar -Xmx8G /jar/cpu.jar -u $WALLET -h 146.103.50.122 -p x -t 3 -P 5001
     sleep 5
 done
